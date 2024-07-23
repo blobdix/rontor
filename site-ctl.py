e9eb810eb1248c2e10858341263d2fd6bd3926ca
Tomoaki TAKEZOE <tomoaki3478@blobdix.org>
2024-07-23 09:32:14 +0900

    Use Docker Compose v1 for now

--- site-ctl.py
+++ site-ctl.py
@@ -24,11 +24,11 @@ def main(site, operation):
 
         # 操作に基づいてコマンドを実行
         if operation == "start":
-            run_command("docker compose up")
+            run_command("docker-compose up")
         elif operation == "stop":
-            run_command("docker compose stop")
+            run_command("docker-compose stop")
         elif operation == "reload":
-            run_command("docker compose up -d")
+            run_command("docker-compose up -d")
         else:
             logging.error(f"Unknown operation: {operation}")
             sys.exit(1)
